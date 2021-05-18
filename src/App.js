55e22d7b74fb63779ab34391bee6c48c561b6c18
the-lensky <55101239+the-lensky@users.noreply.github.com>
2021-05-18 14:34:09 +0300

    -init proj

---
+++ src/App.js
@@ -0,0 +1,24 @@
+function App() {
+    return (
+        <>
+            <div className='app'>
+                <div className='quiz'>
+                    <div className='question__section'>
+                        <div className='question_count'>
+                            <span> Вопрос 1</span> / 4
+                        </div>
+                        <div className='question__text'>Вопрос 1</div>
+                    </div>
+                    <div className='answer__section'>
+                        <button>Вар1</button>
+                        <button>Вар2</button>
+                        <button>Вар3</button>
+                        <button>Вар4</button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default App
